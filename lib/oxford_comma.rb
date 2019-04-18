09eefbe570e0e73451f7129030169f6d3527c9a1
zhenggao cai <caizhengao1992@gmail.com>
2019-04-18 07:08:53 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,6 +3,6 @@ last_string=array.last
 array.pop
 first_string=array.join(", ")
 
-second_string=first_string << ", and #{last_string}"
+second_string=first_string << " and #{last_string}"
 return second_string
 end
