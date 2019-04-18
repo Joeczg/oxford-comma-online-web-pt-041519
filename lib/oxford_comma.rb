4d6d0298663dae3edd8372597d5ad41fc9e4c573
zhenggao cai <caizhengao1992@gmail.com>
2019-04-18 07:23:45 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,15 @@
 def oxford_comma(array)
-last_string=array.last
+
+
+if array.size<2
+first_string=array.join
+
+elsif array.size==2
+first_string=array.join(" and ")
+elsif array.size==3
+last_array=array.last
 array.pop
 first_string=array.join(", ")
-
-second_string=first_string << " and #{last_string}"
+second_string=first_string << " and #{last_array}"
+else last_array=array.last
 end
