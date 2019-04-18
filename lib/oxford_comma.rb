3c2461697031066f1dd1e1545fe8701c5747d5f2
zhenggao cai <caizhengao1992@gmail.com>
2019-04-18 07:08:09 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,7 @@
 def oxford_comma(array)
-
+new_array=array.pop
+first_string=new_array.join(", ")
+last_string=array.last
+second_string=first_string << "and #{last_string}"
+return second_string
 end
